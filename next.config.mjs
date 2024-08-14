3d809794825d80d623f686e183dea5ab955d7782
Jr.Soares <juniorsoaresti@gmail.com>
2024-08-14 12:13:55 -0400

    API wrapper

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains:['github.com']
-    }
+images:{
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'github.com',
+            port: '',
+            pathname: '/**',
+        },
+    ],
+}
 };
 
 export default nextConfig;
