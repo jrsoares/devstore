d5275eb2333e806821cbd3275bb5e7fa427874fc
Jr.Soares <juniorsoaresti@gmail.com>
2024-08-15 11:08:32 -0400

    deploy in github

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { CartWidget } from "@/components/cart-widget";
 import { SearchForm } from "@/components/search-form";
+import { Suspense } from "react";
 
 export default function Header() {
   return (
@@ -10,7 +11,9 @@ export default function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
