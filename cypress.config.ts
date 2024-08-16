b9414316a70fa8aca6fb18fdce1a2636e27954f8
Jr.Soares <juniorsoaresti@gmail.com>
2024-08-15 21:05:36 -0400

    config cypress browser brave

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from "cypress";
+// @ts-ignore
+import execa from "execa";
+const findBrowser = () => {
+  // the path is hard-coded for simplicity
+  const browserPath =
+    "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser";
 
+  // @ts-ignore
+  return execa(browserPath, ["--version"]).then(
+    (result: { stdout: string }) => {
+      // STDOUT will be like "Brave Browser 77.0.69.135"
+      // @ts-ignore
+      const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
+        result.stdout,
+      );
+      const majorVersion = parseInt(version.split(".")[0]);
+
+      return {
+        name: "Brave",
+        channel: "stable",
+        family: "chromium",
+        displayName: "Brave",
+        version,
+        path: browserPath,
+        majorVersion,
+      };
+    },
+  );
+};
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return findBrowser().then((browser: any) => {
+        return {
+          browsers: config.browsers.concat(browser),
+        };
+      });
     },
   },
 });
